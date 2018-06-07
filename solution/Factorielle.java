a292eb6026f0e4ee0cf4c97fbc4afc08821f821f
bastienwcs <bastien@wildcodeschool.fr>
2018-06-07 11:15:11 +0200

    init

---
+++ solution/Factorielle.java
@@ -0,0 +1,9 @@
+class Factorielle {
+
+	public static long facto(long n) {
+		if (n == 0 || n == 1) {
+			return 1;
+		}
+		return facto(n-1) * n; 
+	}
+}
---
+++ solution/FactorielleTest.java
@@ -0,0 +1,19 @@
+import junit.framework.*;
+import org.junit.Test;
+
+public class FactorielleTest extends TestCase {
+
+	@Test
+	public void testTrue() throws Exception {
+		assertEquals(true, true);
+	}
+
+	@Test
+	public void test1() throws Exception {
+		assertEquals(1, Factorielle.facto(0));
+		assertEquals(1, Factorielle.facto(1));
+		assertEquals(2, Factorielle.facto(2));
+		assertEquals(6, Factorielle.facto(3));
+		assertEquals(24, Factorielle.facto(4));
+	}
+}
